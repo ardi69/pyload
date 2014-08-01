b06c646fcda01f2486ac253c59bbd1854843ea62
Walter Purcaro <vuolter@gmail.com>
2014-08-01 16:42:25 +0200

    Bucket and CookieJar from master

--- module/network/CookieJar.py
+++ module/network/CookieJar.py
@@ -1,50 +1,39 @@
 # -*- coding: utf-8 -*-
 
-"""
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License,
-    or (at your option) any later version.
+import Cookie
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, see <http://www.gnu.org/licenses/>.
-
-    @author: mkaay, RaNaN
-"""
-
+from datetime import datetime, timedelta
 from time import time
 
-class CookieJar:
-    def __init__(self, pluginname, account=None):
-        self.cookies = {}
-        self.plugin = pluginname
-        self.account = account
 
-    def addCookies(self, clist):
-        for c in clist:
-            name = c.split("\t")[5]
-            self.cookies[name] = c
+# monkey patch for 32 bit systems
+def _getdate(future=0, weekdayname=Cookie._weekdayname, monthname=Cookie._monthname):
+    dt = datetime.now() + timedelta(seconds=int(future))
+    return "%s, %02d %3s %4d %02d:%02d:%02d GMT" % \
+           (weekdayname[dt.weekday()], dt.day, monthname[dt.month], dt.year, dt.hour, dt.minute, dt.second)
 
-    def getCookies(self):
-        return self.cookies.values()
+Cookie._getdate = _getdate
 
-    def parseCookie(self, name):
-        if name in self.cookies:
-            return self.cookies[name].split("\t")[6]
-        else:
-            return None
 
+class CookieJar(Cookie.SimpleCookie):
     def getCookie(self, name):
-        return self.parseCookie(name)
+        return self[name].value
 
-    def setCookie(self, domain, name, value, path="/", exp=time()+3600*24*180):
-        s = ".%s	TRUE	%s	FALSE	%s	%s	%s" % (domain, path, exp, name, value)
-        self.cookies[name] = s
+    def setCookie(self, domain, name, value, path="/", exp=None, secure="FALSE"):
+        self[name] = value
+        self[name]["domain"] = domain
+        self[name]["path"] = path
 
-    def clear(self):
-        self.cookies = {}
+        # Value of expires should be integer if possible
+        # otherwise the cookie won't be used
+        if not exp:
+            expires = time() + 3600 * 24 * 180
+        else:
+            try:
+                expires = int(exp)
+            except ValueError:
+                expires = exp
+
+        self[name]["expires"] = expires
+        if secure == "TRUE":
+            self[name]["secure"] = secure
